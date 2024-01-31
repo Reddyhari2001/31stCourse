bfbfbd8ee0bdac1c824327dcb1683aab87ea1f8f
hariprasafdreddy <hariprasad1999@gmail.com>
2024-01-31 15:08:18 +0530

    Add project files.

---
+++ 31stCourse/Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using _31stCourse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _31stCourse.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        static List<Course> Courses = new List<Course>()
+        {
+            new Course{CId=101,CName="Programming",CFee=5000,Technology="C#",StartDate = DateTime.Now, EndDate=DateTime.Now.AddMonths(3)},
+            new Course{CId=102,CName="WebDevelopment",CFee=1000,Technology="Html,Css",StartDate = DateTime.Now, EndDate=DateTime.Now.AddMonths(1)}
+        };
+        public ActionResult Index()
+        {
+            return View(Courses);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Course());
+        }
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                Courses.Add(course);
+                return RedirectToAction("Index");
+            }
+            return View(course);
+        }
+    }
+}
---
+++ 31stCourse/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _31stCourse.Models
+{
+    public class Course
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+        public decimal CFee { get; set; }
+        public string Technology { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
